244cfb3508514c74cf5fa3a09c825d67d1acb988
Thang Tran <thangt@metropolia.fi>
2020-07-11 00:43:27 +0300

    Draws a circle made up of squares (2.18)

---
+++ Python/2.18-whichDataType.py
@@ -0,0 +1,15 @@
+# This program draws a circle made up of squares
+import turtle
+t = turtle.Turtle()
+t.speed(0)
+
+def circlify(length, color, sides):
+    t.color(color)
+    t.penup()
+    t.back(length / 2)
+    t.pendown()
+    for side in range(100):
+        t.forward(length)
+        t.right(360/sides + 2)
+
+circlify(150, "gold", 4)
