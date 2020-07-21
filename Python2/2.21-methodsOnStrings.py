c4a34562ec1ce696aad0060cde79460bf3b3903b
Thang Tran <pthang1102@gmail.com>
2020-07-21 15:45:12 +0300

    Several built-in methods for Strings (2.21)

---
+++ Python2/2.21-methodsOnStrings.py
@@ -0,0 +1,14 @@
+# Several built-in methods for Strings
+
+# For example. This should print "True"
+if ("apple".startswith("app")):
+    print("True.")
+else:
+    print("False.")
+
+# There are more methods, such as: 
+# isdigit, iswhitespace, iscaps, islower, 
+# isspace, isnumber, etc.
+
+#CHECK MORE AT: 
+# https://docs.python.org/3/library/stdtypes.html#string-methods
