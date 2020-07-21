a8daa944cde93b35e6e4a4ea1787f5448f10dbc9
Thang Tran <pthang1102@gmail.com>
2020-07-21 15:30:31 +0300

    Practice convert str-values to int-values (2.17)

---
+++ Python2/2.17-stringsToNumbersToStrings.py
@@ -0,0 +1,6 @@
+# Practice convert str-values to int-values
+num1 = int(input("Enter a number: ")) # Converts input from str to int
+num2 = int(input("Enter another number: "))
+num3 = int(input("Enter a third number: "))
+sum = num1 + num2 + num3
+print(f"{num1} + {num2} + {num3} = {sum}")
