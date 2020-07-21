48646da5b44706856d57740dc031d54eb6d79c28
Thang Tran <pthang1102@gmail.com>
2020-07-21 15:17:50 +0300

    Use f-string to simplify string-concatenation (2.16)

---
+++ Python2/2.16-f-strings.py
@@ -0,0 +1,6 @@
+# Use f-string to simplify string-concatenation
+
+name = input("What is your name?\n")
+print(f"Hi {name}! It's very nice to meet you.\n")
+color = input("What is your favorite color?\n")
+print(f"Oh, {color} is a lovely!")
