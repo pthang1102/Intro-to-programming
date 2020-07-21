7e39d3d0e4f860cfdd0dc4929e5de8e169d35e6e
Thang Tran <pthang1102@gmail.com>
2020-07-21 15:05:18 +0300

    This program takes a string as input and returns that string with "ly" appended to it (2.14)

---
+++ Python2/2.14-concatenation.py
@@ -0,0 +1,4 @@
+# This program takes a string as input and returns that string with "ly" appended to it
+str = input("Please enter a noun or verb: ")
+final_str = str + "ly"
+print("Your final string is:", final_str)
