598d48fbdfbc696da30d1ad6bbedbe2e6e568d47
Thang Tran <pthang1102@gmail.com>
2020-07-22 15:45:05 +0300

    Basic password-checker and count-down using while-loops (3.5)

---
+++ Python2/3.5-whileLoops.py
@@ -0,0 +1,13 @@
+# Gets used to while-loops
+
+# Basic password checker using while-loop
+password = ''
+while password != 'helloworld':
+    password = input('Please enter the password: ')
+
+# Count down using while-loop
+count = 10
+while count > 0:
+    print(count)
+    count -= 1
+print('Blastoff!')
